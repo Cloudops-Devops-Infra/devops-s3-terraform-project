c6a26a8203a963e1ccbec288a762565cf8191719
Subash Patra <patrasubash92@gmail.com>
2026-06-15 18:15:32 +0530

    Initial DevOps project

---
+++ terraform-s3/main.tf
@@ -0,0 +1,26 @@
+﻿terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = "test"
+  secret_key = "test"
+  region     = "us-east-1"
+
+  endpoints {
+    s3 = "http://localhost.floci.io:4566"
+  }
+
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "terraform-demo-bucket"
+}
